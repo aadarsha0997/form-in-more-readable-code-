0c53327860a7078429a02e175a216398710d3da5
Ayush <aadarsha0997@gmail.com>
2024-11-07 13:28:25 +0545

    initilizing git

---
+++ reviews/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Review
+####### form extend from form class ##############
+
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(required=True, label="Your Name",max_length=100,error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length":"Please enter a shorter name!"
+#     })
+#     review_text=forms.CharField(label="Your Feedback",widget=forms.Textarea,max_length=200)
+#     rating=forms.IntegerField(label="Your Rating",min_value=1,max_value=5)
+
+
+######## Form extended from modelform class #######
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model= Review
+        #### field=['user_name','review_text' ]####  select which field of model to include in form one by one
+        field='__all__' ### all field of model should be include 
+        #### exclude=['use_name'] #### which field of modle should be excluded in form
+        labels=
+
---
+++ reviews/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import ReviewForm
+from .models import Review
+
+# Create your views here.
+
+######### Defult way to make views without forms.py ######
+# def review(request):
+#     if request.method == 'POST':
+#         entered_data=request.POST['username'] ### post is dictionary and username is name to form field we assigne so we can save mail in diffferent veriable and username in deifferent veriable
+#         print(entered_data)
+#         return HttpResponseRedirect("thank-you")
+
+#     return render(request,"reviews/review.html")
+
+########## Way to make validation using form.py extending from form class
+
+def review(request):
+    if request.method=='POST':
+
+        form = ReviewForm(request.POST) #assining veriable , not pointing like class base view & POST is collected data from user in dictionary
+        if form.is_valid(): ### is_valid is built_in function in form class 
+            review=Review(user_name=form.cleaned_data['user_name'],
+                          review_text=form.cleaned_data['review_text'],
+                          rating=form.cleaned_data['rating']) ## cleaned_data is also function in from class to print all data
+            review.save()
+            return HttpResponseRedirect("/thank-you")
+    else:
+        form=ReviewForm()
+    return render(request, "reviews/review.html",{
+        "form":form,
+    })
+
+
+######### manual veladition on form to not be empty and longer then 100 character. ######
+
+# def review(request):
+#     if request.method == 'POST':
+#         entered_data=request.POST['username'] ### post is dictionary and username is name to form field we assigne so we can save mail in diffferent veriable and username in deifferent veriable
+#         print(entered_data)
+#         return HttpResponseRedirect("thank-you")
+
+#     return render(request,"reviews/review.html")
+
+
+def thank_you(request):
+    
+    return render(request,"reviews/thank_you.html")
